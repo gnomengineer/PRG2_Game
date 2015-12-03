e1bb8e9649f612d7a30c8a1f8e5627b73892528b
metterlin <metterlin@GICT-WS004.gict.ch>
2015-12-03 13:56:10 +0100

    Added some Code

--- src/GameOptionsView.java
+++ src/GameOptionsView.java
@@ -8,7 +8,11 @@
  *
  * @author metterlin
  */
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.*;
+import static javax.swing.JFrame.*;
 
 /**
  *
@@ -18,21 +22,69 @@ public class GameOptionsView {
 
     JFrame jframeOptions;
     JPanel jPanelOptions;
-    private int width=100;
-    private int height= 50;
+    JPanel jPanelGameMode;
+    JPanel jPanelField;
+    JLabel jLabelGameMode;
+    JLabel jLabelFieldSize;
+    JLabel jLabelx;
+    JLabel jLabely;
+    ButtonGroup bgrpRadioButtons;
+    JRadioButton jRadioSingle;
+    JRadioButton jRadioMulti;
+    JSlider jSliderX;
+    JSlider jSliderY;
+    
+      
+    private static final Dimension size= new Dimension(800,200);
+    
     
     
     
     public GameOptionsView(){
         jframeOptions=new JFrame("Options Dots and Boxes");
+        jPanelOptions= new JPanel();
+        jLabelGameMode = new JLabel("Game Mode");
+        jLabelFieldSize= new JLabel("Field Size");
+        bgrpRadioButtons = new ButtonGroup();
+        jRadioSingle = new JRadioButton("SinglePlayer");
+        jRadioMulti= new JRadioButton("MultiPlayer");
+        jPanelGameMode = new JPanel();
+        jPanelField = new JPanel();
+        
         setup();
     }
     public void setup(){
-        jframeOptions.setSize(width, height);
+        jframeOptions.setSize(size);
+        jframeOptions.setPreferredSize(size);
         jPanelOptions = new JPanel();
         jframeOptions.add(jPanelOptions);
+        jframeOptions.setDefaultCloseOperation(EXIT_ON_CLOSE);
         jframeOptions.setVisible(true);
-        //jframeOptions.
+        
+        //JSlider
+        
+        
+        //jPanel
+        jPanelOptions.setLayout(new GridLayout(1,2));
+        
+        //Radio Buttons
+        bgrpRadioButtons.add(jRadioSingle);
+        bgrpRadioButtons.add(jRadioMulti);
+        
+        //Setup JPanel
+        jPanelOptions.add(jPanelGameMode);
+        jPanelOptions.add(jPanelField);
+        jPanelGameMode.setLayout(new GridLayout(0,1));
+        jPanelField.setLayout(new GridLayout(0,1));
+        
+        //Fill Panells
+        jPanelGameMode.add(jRadioSingle);
+        jPanelGameMode.add(jRadioMulti);
+        
+        
+        
+        
+        
         
         
     }
