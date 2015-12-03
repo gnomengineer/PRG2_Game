ad2a00ce948d7235889d38abfb0caa87c77ceae9
André <wgna92@gmail.com>
2015-12-03 13:07:16 +0100

    Interface GameView implementiert

--- src/GameView.java
+++ src/GameView.java
@@ -11,31 +11,33 @@ public class GameView implements GameViewInterface {
 
     @Override
     public void startGameView(int width, int height) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // hier grafikobjekte aufbaufbauen, welche du auch zuordnen kannst.
     }
 
     @Override
     public void startOptionView() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // die optionview anzeigen
     }
 
     @Override
     public void drawLine(int startX, int startY, int endX, int endY, boolean isOpponent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // anhand isOppenent eine am anfang festgelegte Farbe verwenden
     }
 
     @Override
     public void updatePlayerState(int points) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // das TextControl updaten
     }
 
     @Override
     public void updateOpponentState(int points) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // das TextControl updaten
     }
 
     @Override
     public void showMessage(String message, MessageTypeEnum messageType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        // siehe MessageType
+        // warning und information als MessageBox anzeigen, restart und end kannst du das spielfeld zurücksetzen
+        // jedoch noch nicht ganz klar, was... evtl. restart heisst, das das Spiel von vorne beginnt, alle grafikobjekte reseten
     }
 }
