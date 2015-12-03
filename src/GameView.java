01ae257603b2e71e8d34734370d951db3c17addc
André <wgna92@gmail.com>
2015-12-03 11:35:39 +0100

    Interface GameView

--- src/GameView.java
+++ src/GameView.java
@@ -1,8 +1,41 @@
 
+import Interfaces.GameViewInterface;
+import Interfaces.MessageTypeEnum;
+
+
 /**
  *
  * @author Andre
  */
-public class GameView {
-    
+public class GameView implements GameViewInterface {
+
+    @Override
+    public void startGameView(int width, int height) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void startOptionView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void drawLine(int startX, int startY, int endX, int endY, boolean isOpponent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updatePlayerState(int points) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateOpponentState(int points) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void showMessage(String message, MessageTypeEnum messageType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
