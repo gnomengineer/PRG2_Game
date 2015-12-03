f7bddd15a1ec7d69bd5d6d39a40e3cb616e05e0c
metterlin <metterlin@GICT-WS004.gict.ch>
2015-12-03 10:43:50 +0100

    test commint message

--- src/GameOptionsView.java
+++ src/GameOptionsView.java
@@ -8,6 +8,35 @@
  *
  * @author metterlin
  */
+import javax.swing.*;
+
+/**
+ *
+ * @author metterlin
+ */
 public class GameOptionsView {
+
+    JFrame jframeOptions;
+    JPanel jPanelOptions;
+    private int width=100;
+    private int height= 50;
     
+    
+    
+    public GameOptionsView(){
+        jframeOptions=new JFrame("Options Dots and Boxes");
+        setup();
+    }
+    public void setup(){
+        jframeOptions.setSize(width, height);
+        jPanelOptions = new JPanel();
+        jframeOptions.add(jPanelOptions);
+        jframeOptions.setVisible(true);
+        jframeOptions.
+        
+        
+    }
+    public static void main(String[] args){
+        GameOptionsView optionView1 = new GameOptionsView();
+    }
 }
