ddecd7dfeda8a6129f6b193bf379a641644569f4
troopa <troopa.der.pate@gmail>
2015-12-04 10:00:56 +0000

    corrected AIController interface implementation

--- src/AIController.java
+++ src/AIController.java
@@ -1,12 +1,15 @@
+import Enums.MessageTypeEnum;
 import GameObjects.Line;
 import GameObjects.Map;
+import Interfaces.ObserverInterface;
 import Interfaces.OpponentInterface;
+import Interfaces.SubjectInterface;
 
 /**
  *
  * @author Andre
  */
-public class AIController implements OpponentInterface 
+public class AIController implements OpponentInterface,SubjectInterface
 {
     private Map map;
     
@@ -16,8 +19,24 @@ public class AIController implements OpponentInterface
     }
 
     @Override
-    public Line makeAMove()
-    {
+    public void sendGameInfo(Line selectedLine) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public void sendGameInfo(MessageTypeEnum msg) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void registerObserver(ObserverInterface observer) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void notifyObserver() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
 }
