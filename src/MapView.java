880fc2ec71564690ea8055472788a4ddbab12470
Martin <Martin@192.168.192.49>
2015-12-04 17:11:26 +0100

    Added Points to map

--- src/MapView.java
+++ src/MapView.java
@@ -8,17 +8,24 @@ import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import javax.swing.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 /**
  *
  * @author Martin
  */
 public class MapView extends JPanel {
     private int angle = 0;
+    private int mWidth;
+    private int mHeight;
+    Graphics2D g2d;
+    Rectangle2D.Double[][] points;
    
-    public MapView(){
-        setBackground(Color.red);
+    public MapView(int mWidth, int mHeight){
+        //setBackground(Color.red);
         add(new JButton("test"));
-        
+        this.mWidth = mWidth;
+        this.mHeight= mHeight;
         
     }
     
@@ -28,9 +35,30 @@ public class MapView extends JPanel {
      @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g.create();
-        g2d.draw(new Line2D.Double(30, 30, 30, 60));
-
-
+        g2d = (Graphics2D) g.create();
+        initPoints();
+        drawPoints();
+    }
+    private void initPoints(){
+        int ycoordinates=10;
+        int xcoordinates=10;
+        points= new Rectangle2D.Double[mWidth+1][mHeight+1];
+        for(int i=0; i<=mHeight;i++){
+            for(int y=0; y<=mWidth; y++){
+                points[i][y]= new Rectangle2D.Double(xcoordinates-0.5, ycoordinates-0.5, 0.5, 0.5);
+                xcoordinates=xcoordinates+30;
+            }
+            xcoordinates=10;
+            ycoordinates =ycoordinates+30;
+        }
+        
+    }
+    
+    private void drawPoints(){
+        for(int i=0; i<=mHeight;i++){
+            for(int y=0; y<=mWidth; y++){
+                g2d.draw(points[i][y]);
+            }
+        }
     }
 }
