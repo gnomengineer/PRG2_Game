dcb956e80c4d080bc5dc30ad7c8c615015cebf03
Martin <Martin@192.168.192.49>
2015-12-12 14:03:44 +0100

    Changed LayoutManager of Score to Change FontSize properly, Implemented Whos Turn -> not working correct yet

--- src/Views/GameView.java
+++ src/Views/GameView.java
@@ -40,6 +40,7 @@ public class GameView implements GameViewInterface, SubjectInterface {
     JPanel jPanelCenter;
     JPanel jPanelScoreView;
     JPanel jPanelWest;
+    JPanel jPanelTurnView;
     
     //JLabel
     JLabel jLabelScore;
@@ -47,6 +48,7 @@ public class GameView implements GameViewInterface, SubjectInterface {
     JLabel jLabelScoreOpponent;
     JLabel jLabelScoreOwnPoints;
     JLabel jLabelScoreOpponentPoints;
+    JLabel jLabelTurn;
     ObserverInterface observer;
     
     //JFilechooser
@@ -58,6 +60,9 @@ public class GameView implements GameViewInterface, SubjectInterface {
     Dimension size;
     MapView map1;
     
+    //Fonts
+    Font fontScore;
+    
     public GameView(){
         jMBGameView= new JMenuBar();      
         jFrameGameView = new JFrame("Dots and Boxes");
@@ -68,16 +73,20 @@ public class GameView implements GameViewInterface, SubjectInterface {
         jMISave = new JMenuItem("Save");
         jMIClose = new JMenuItem("Close");
         jMIOpen = new JMenuItem("Open");
-        size = new Dimension(800, 800);
+        size = new Dimension(400, 200);
         jPanelCenter=new JPanel();
         jPanelScoreView=new JPanel();
         jPanelWest = new JPanel();
+        jPanelTurnView=new JPanel();
         jLabelScore=new JLabel("Score");
         jLabelScoreOwn = new JLabel("Your Points:");
         jLabelScoreOpponent = new JLabel ("Opponent Points:  ");
         jLabelScoreOwnPoints = new JLabel("0");
         jLabelScoreOpponentPoints = new JLabel("0");
+        jLabelTurn= new JLabel("Turn");
         jFChooser = new JFileChooser();
+        fontScore = new Font("Calibri", Font.BOLD, 20);
+        
         setup();
         
         jFChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -94,7 +103,7 @@ public class GameView implements GameViewInterface, SubjectInterface {
         jFrameGameView.setDefaultCloseOperation(EXIT_ON_CLOSE);
         jFrameGameView.add(jPanelCenter,BorderLayout.CENTER); 
         jFrameGameView.add(jPanelWest, BorderLayout.WEST);
-        
+        jFrameGameView.add(jPanelTurnView, BorderLayout.NORTH);
         //Menubar
         jFrameGameView.setJMenuBar(jMBGameView);
         jMBGameView.add(jMGame);
@@ -134,35 +143,45 @@ public class GameView implements GameViewInterface, SubjectInterface {
           JOptionPane.showMessageDialog(null, "Author: \nBläsi\nSamuel Degelo\nHairy Föhn\nMartin Etterlin", "About", JOptionPane.INFORMATION_MESSAGE);
         });
         
-        
-        
-        
-        
-        
-        
-        
         //jPanelCenter
         jPanelCenter.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         jPanelCenter.setLayout(new FlowLayout() );
         
         //jPanelWest
-        jPanelWest.setLayout(new FlowLayout());
-        jPanelWest.add(jPanelScoreView);
-        jPanelWest.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        jPanelWest.setLayout(new FlowLayout() );
+        jPanelWest.setBorder(BorderFactory.createEmptyBorder(20 , 20, 20, 20));
+        jPanelWest.add(jPanelScoreView);;
+        
+        //jFrameGameView.add(jPanelTurnView);
         
         //jPanelScoreView
-        jPanelScoreView.setLayout(new GridLayout(2,2));
-        jPanelScoreView.add(jLabelScoreOwn);
-        jPanelScoreView.add(jLabelScoreOwnPoints);
-        jPanelScoreView.add(jLabelScoreOpponent);
-        jPanelScoreView.add(jLabelScoreOpponentPoints);
+        jPanelScoreView.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx=0;
+        gbc.gridy=0;
+        gbc.anchor = GridBagConstraints.LINE_START;
+        jPanelScoreView.add(jLabelScoreOwn,gbc);
+        gbc.gridx++;
+        jPanelScoreView.add(jLabelScoreOwnPoints,gbc);
+        gbc.gridy++;
+        gbc.gridx--;
+        jPanelScoreView.add(jLabelScoreOpponent, gbc);
+        gbc.gridx++;
+        jPanelScoreView.add(jLabelScoreOpponentPoints, gbc);
+        
+        
+        //jPanelTurnView
+        jPanelTurnView.add(jLabelTurn);
         
         //jLabel
         jLabelScoreOpponent.setForeground(Color.blue);
         jLabelScoreOwn.setForeground(Color.red);
         jLabelScoreOpponentPoints.setForeground(Color.blue);
         jLabelScoreOwnPoints.setForeground(Color.red);
-        
+        jLabelScoreOpponent.setFont(fontScore);
+        jLabelScoreOpponentPoints.setFont(fontScore);
+        jLabelScoreOwn.setFont(fontScore);
+        jLabelScoreOwnPoints.setFont(fontScore);
     }
 
     
@@ -242,17 +261,11 @@ public class GameView implements GameViewInterface, SubjectInterface {
     @Override
     public void updatePlayerTurn(boolean isOpponent) {
         if(isOpponent){
-            jLabelScoreOpponent.setOpaque(true);
-            jLabelScoreOpponent.setBackground(Color.yellow);
-            jLabelScoreOwn.setOpaque(false);
+            jLabelTurn.setText("Opponent turn!");
         }
         else{
-            jLabelScoreOwn.setOpaque(true);
-            jLabelScoreOwn.setBackground(Color.yellow);
-            jLabelScoreOpponent.setOpaque(false);
-            jFrameGameView.repaint();
-        }
-       
+            jLabelTurn.setText("Your turn!");
+        } 
     }
 
     @Override
